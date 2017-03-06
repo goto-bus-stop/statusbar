0a0414ff9a1cd6cffbea2c17289439e516dbfa16
René Kooi <renee@kooi.me>
2017-03-06 16:34:26 +0100

    Add dzen integration.

---
+++ packages/dzen/index.js
@@ -0,0 +1,71 @@
+const spawn = require('child_process').spawn
+const split = require('split')
+
+const defaultOptions = {}
+
+module.exports = (options = {}) => (bar) => {
+  options = Object.assign({}, defaultOptions, options)
+
+  const dz = spawn('dzen2', ['-fn', 'monospace'])
+
+  /**
+   * Process an incoming action.
+   *
+   * Actions come in as button,block pairs, eg:
+   *
+   *   "1 1" is a left click on block #1
+   *   "2 7" is a right click on block #7
+   *
+   * @param {string} chunk
+   */
+  function oninput (chunk) {
+    const [button, id] = chunk.split(' ')
+    bar.input.write({ button: parseInt(button, 10), instance: id })
+  }
+
+  /**
+   * Add an action handler to the given dzen2 string.
+   *
+   * @param {number} button
+   * @param {Object} block
+   * @param {string} text
+   */
+  function addAction (button, block, text) {
+    return `^ca(${button},echo ${button} ${block.instance})${text}^ca()`
+  }
+
+  /**
+   * Serialize a block formatted according to the i3bar protocol to a
+   * dzen2-compatible string.
+   *
+   * @param {Object} block
+   * @return {string}
+   */
+  function serializeBlock (block) {
+    let result = block.full_text
+
+    // Style
+    if (block.color) result = `^fg(${block.color})${result}^fg()`
+    if (block.background) result = `^bg(${block.background})${result}^bg()`
+
+    // Events
+    if (block._onclick) result = addAction(1, block, result)
+    if (block._onrightclick) result = addAction(2, block, result)
+
+    return result
+  }
+
+  /**
+   * Output a status bar.
+   *
+   * @param {Array<Object>} chunk A status bar.
+   */
+  function onoutput (chunk) {
+    const text = chunk.map(serializeBlock).join(' | ')
+
+    dz.stdin.write(text + '\n')
+  }
+
+  bar.output.on('data', onoutput)
+  dz.stdout.pipe(split()).on('data', oninput)
+}
---
+++ packages/dzen/readme.md
@@ -0,0 +1,15 @@
+# @statusbar/dzen
+
+dzen integration for [Statusbar](https://github.com/goto-bus-stop/statusbar).
+
+## Usage
+
+```js
+const dzen = require('@statusbar/dzen')
+
+bar.use(dzen())
+```
+
+## License
+
+[MIT](../../LICENSE)
