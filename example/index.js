20272b665804566044a7723fce06c15d438b6b19
René Kooi <renee@kooi.me>
2017-03-09 12:21:42 +0100

    Add example

---
+++ example/index.js
@@ -0,0 +1,32 @@
+const bar = require('../packages/statusbar')()
+const dzen = require('../packages/dzen')
+const date = require('../packages/date')
+const brightness = require('../packages/brightness')
+const battery = require('../packages/battery')
+const diskusage = require('../packages/diskusage')
+const volume = require('../packages/volume')
+const whoami = require('../packages/whoami')
+const text = require('../packages/text')
+const wifi = require('../packages/wifi')
+
+bar.use(text({
+  background: 'white',
+  color: 'red',
+  text: 'Shutdown',
+  onclick: () => process.exit(0)
+}))
+
+bar.use(wifi())
+bar.use(brightness())
+bar.use(battery())
+bar.use(diskusage('/dev/sda2'))
+bar.use(diskusage('/dev/sdb2'))
+bar.use(date())
+bar.use(volume())
+bar.use(text('hello'))
+bar.use(whoami({
+  background: 'rebeccapurple',
+  color: 'white'
+}))
+
+bar.use(dzen())
---
+++ example/readme.md
@@ -0,0 +1,8 @@
+# Statusbar Example
+
+An example statusbar configuration using most of the available blocks and
+options, and outputting to dzen.
+
+Run `npm start` in this directory to run.
+
+[View source](./index.js)
