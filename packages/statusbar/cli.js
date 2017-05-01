01b87e48fc12bd7073f138b9cf11a11a4b46217e
Renée Kooi <renee@kooi.me>
2017-05-01 19:26:02 +0200

    Add a default config file location.

--- packages/statusbar/cli.js
+++ packages/statusbar/cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const fs = require('fs')
 const path = require('path')
+const xdg = require('xdg-basedir')
 
-const config = process.argv[2]
+const config = process.argv[2] || path.join(xdg.config, 'statusbar')
 
 if (!config) {
   console.log('Usage: statusbar <config-file>')
